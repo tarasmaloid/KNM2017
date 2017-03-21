9456b1202cc8d1d6dd18b3d9fd11c4baf4cbaae1
AndriyMahera <andriymahera@gmail.com>
2017-03-21 23:06:02 +0200

    Laba2

---
+++ Maherovsky/Maherovsky_lab2/Branins.m
@@ -0,0 +1,3 @@
+function z = Branins(x)
+z=(x(2)-5.1/4/pi^2*x(1)^2+5*x(1)/pi-6)^2+10*(1-1/8/pi)*cos(x(1))+10
+end
---
+++ Maherovsky/Maherovsky_lab2/SixHump.m
@@ -0,0 +1,10 @@
+function z = SixHump(x)
+x1 = x(1);
+x2 = x(2);
+
+term1 = (4-2.1*x1^2+(x1^4)/3) * x1^2;
+term2 = x1*x2;
+term3 = (-4+4*x2^2) * x2^2;
+
+z = term1 + term2 + term3;
+end
