584426317a9d7d105c365f05bc8de954d74913c1
Unknown <laska.g95@ukr.net>
2017-03-21 02:07:59 +0200

    Виконано лаб2
    * файли fun1, fun_2 ... - файлиз обраними функціями
    * ga_var1, ga_var2 - налаштування ga згідно з варіантом
    * ga_static - повертає статистичні дані у вигляді масиву (приклад таблички у звіті)
    * run - запуск програми. результати не виводяться на екран, але їх можна переглянути засобами matlab

---
+++ laska/laska_lab2/ga_var1.m
@@ -0,0 +1,15 @@
+function [ time, fval ] = ga_var1( func, nvars, range, population_size, generations )
+    
+options = gaoptimset();
+options = gaoptimset(options, 'SelectionFcn', @selectiontournament);
+options = gaoptimset(options, 'CrossoverFcn', @crossoversinglepoint);
+options = gaoptimset(options, 'MutationFcn', @mutationgaussian);
+%options = gaoptimset(options, 'PopInitRange', range);
+options = gaoptimset(options, 'Generations', generations);
+options = gaoptimset(options, 'PopulationSize', population_size);
+
+tic
+[fx, fval] = ga(func, nvars, options);
+time = toc;
+
+end
