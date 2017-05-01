5a2574936bf2a2cc6d5d9c3b02da4248657d8eb6
Unknown <laska.g95@ukr.net>
2017-05-01 11:25:07 +0300

    Поправлено звіт

--- laska/laska_lab2/ga_var2.m
+++ laska/laska_lab2/ga_var2.m
@@ -4,9 +4,10 @@ options = gaoptimset();
 options = gaoptimset(options, 'SelectionFcn', @selectionroulette);
 options = gaoptimset(options, 'CrossoverFcn', @crossoverintermediate);
 options = gaoptimset(options, 'MutationFcn', @mutationuniform);
-options = gaoptimset(options, 'PopInitRange', range);
+% options = gaoptimset(options, 'PopInitRange', range);
 options = gaoptimset(options, 'Generations', generations);
 options = gaoptimset(options, 'PopulationSize', population_size);
+% options = gaoptimset(options, 'PlotFcns', {@gaplotbestf, @gaplotdistance});
 
 tic
 [x, fval] = ga(func, nvars, options);
